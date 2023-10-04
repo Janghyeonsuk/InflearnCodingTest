e4571c7197d23bd76b6a959819370a049ecf6ef8
JangHyeonSuk <vkdnjdjxor@naver.com>
2023-10-05 01:00:36 +0900

    최종 commit

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,13 @@
 import java.util.Scanner;
 
 public class Main {
-
-    public void solution(String str) {
+    public void solution(int str) {
     }
 
     public static void main(String[] args) {
         Main main = new Main();
         Scanner in = new Scanner(System.in);
-        String str = in.nextLine();
+        int n = in.nextInt();
+
     }
 }
