7ab59cd28df09bb01ef58d6f5ac52a9672ff6ec4
JangHyeonSuk <vkdnjdjxor@naver.com>
2023-10-28 13:53:48 +0900

    Section7((Recursive) DFS) 최단거리, 인접행렬, 인접리스트, BFS) 최단거리, 인접리스트 ) commit

--- src/CodingTest/RecursiveAndTreeAndGraph/Recursive_3.java
+++ src/CodingTest/RecursiveAndTreeAndGraph/Recursive_3.java
@@ -4,18 +4,23 @@ import java.util.Scanner;
 
 public class Recursive_3 {
     // 3. 팩토리얼
-    public int factorial(int n) {
+    static int n;
+    static int[] num;
 
+    public int DFS(int n) {
+        if (num[n] > 0) return num[n];
         if (n == 1)
-            return 1;
-        else
-            return n * factorial(n - 1);
+            return num[n] = 1;
+        else {
+            return num[n] = n * DFS(n - 1);
+        }
     }
 
     public static void main(String[] args) {
         Recursive_3 main = new Recursive_3();
         Scanner in = new Scanner(System.in);
-        int n = in.nextInt();
-        System.out.print(main.factorial(n));
+        n = in.nextInt();
+        num = new int[n + 1];
+        System.out.println(main.DFS(n));
     }
 }
